a7373df9882053110d75092b5e805573bc07ac59
kanako0424 <budoukan0424@gmail.com>
2024-08-01 21:48:27 +0900

    fix frontend

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,40 +1,50 @@
-import { Box, Container, Flex, Text } from "@chakra-ui/react"
+import { Box, Button, Container, Flex, Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import CreateUserModal from "./CreateUserModal";
+import PropTypes from 'prop-types';
 
-function NavBar() {
-    const { colorMode, toggleColorMode } = useColorMode();
 
-  return (
-    <Container maxW={"900px"}>
-        <Box px={4} my={4} borderRadius={5} bg={useColorModeValue("gray.200", "gray.700")}>
+const Navbar = ({ setUsers }) => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	return (
+		<Container maxW={"900px"}>
+			<Box px={4} my={4} borderRadius={5} bg={useColorModeValue("gray.200", "gray.700")}>
+				<Flex h='16' alignItems={"center"} justifyContent={"space-between"}>
+					{/* Left side */}
+					<Flex
+						alignItems={"center"}
+						justifyContent={"center"}
+						gap={3}
+						display={{ base: "none", sm: "flex" }}
+					>
+						<img src='/react.png' alt='React logo' width={50} height={50} />
+						<Text fontSize={"40px"}>+</Text>
+						<img src='/python.png' alt='Python logo' width={50} height={40} />
+						<Text fontSize={"40px"}>=</Text>
 
-            <Flex 
-                h={"16"}
-                alignItems={"center"}
-                justifyContent={"space-between"}
-            >
-                <Flex
-                    alignItems={"center"}
-                    justifyContent={"center"}
-                    gap={3}
-                    display={{base:"none", sm:"flex"}}
-                >
-                    <img src="/react.png" alt="React Logo" width={50} height={50}/>
-                    <Text fontSize={"40px"}>+</Text>
-                    <img src="/python.png" alt="Python Logo" width={50} height={40}/>
-                    <Text fontSize={"40px"}>=</Text>
-                    <img src="/explode.png" alt="Explode head" width={45} height={45}/>
+						<img src='/explode.png' alt='Explode head' width={45} height={45} />
+					</Flex>
+					{/* Right side */}
+					<Flex gap={3} alignItems={"center"}>
+						<Text fontSize={"lg"} fontWeight={500} display={{ base: "none", md: "block" }}>
+							BFFship 🔥
+						</Text>
 
-                </Flex>
+						<Button onClick={toggleColorMode}>
+							{colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+						</Button>
+						<CreateUserModal setUsers={setUsers} />
+					</Flex>
+				</Flex>
+			</Box>
+		</Container>
+	);
+};
 
-                <Flex gap={3} alignItems={"center"}>
-                    <Text fontSize={"lg"} fontWeight={} ></Text>
-                </Flex>
 
-            </Flex>
+Navbar.propTypes = {
+	setUsers: PropTypes.func.isRequired,
+};
 
-        </Box>
-    </Container>
-  )
-}
-
-export default NavBar
+export default Navbar;
